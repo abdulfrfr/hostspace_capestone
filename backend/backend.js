7711673648c7c9f11f1382f4e98d6f99ae470983
abdulfrfr <abdulmails112@gmail.com>
2024-03-15 12:26:26 +0100

    helm chart modification

--- backend/backend.js
+++ backend/backend.js
@@ -1,34 +1,55 @@
 const express = require('express');
 const cors = require('cors');
-
+const mongoose = require('mongoose');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-
-
+const MONGODB_URL = process.env.MONGODB_URL
 
 app.use(cors());
 app.use(express.json());
 
-// Sample JSON data
-let jsonData = {
-  messages: []
-};
-
-// GET endpoint to retrieve JSON data
-app.get('/data', (req, res) => {
-  res.json(jsonData);
+// Connect to MongoDB using Mongoose
+mongoose.connect(`mongodb://${MONGODB_URL}/data_base`, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}).then(() => {
+  console.log('Connected to MongoDB');
+}).catch((err) => {
+  console.error('Error connecting to MongoDB:', err.message);
 });
 
-// POST endpoint to add data to JSON
-app.post('/data', (req, res) => {
+// Define Mongoose Schema
+const messageSchema = new mongoose.Schema({
+  message: String
+});
+
+const Message = mongoose.model('Message', messageSchema);
+
+// GET endpoint to retrieve data from MongoDB
+app.get('/data', async (req, res) => {
+  try {
+    const messages = await Message.find();
+    res.json(messages);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+// POST endpoint to add data to MongoDB
+app.post('/data', async (req, res) => {
   const { message } = req.body;
   if (!message) {
     return res.status(400).json({ error: 'Message is required' });
   }
 
-  jsonData.messages.push(message);
-  res.json({ message: 'Data added successfully' });
+  try {
+    const newMessage = new Message({ message });
+    await newMessage.save();
+    res.json({ message: 'Data added successfully' });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
 });
 
 // Start the server
---
+++ capestone-chart/templates/helpers.tpl
@@ -0,0 +1,11 @@
+{{- define "frontend-labels" }}
+    app: frontend-app
+{{- end }}
+
+{{- define "backend-labels" }}
+    app: backend-app
+{{- end }}
+
+{{- define "db-labels" }}
+    app: db-app
+{{- end }}

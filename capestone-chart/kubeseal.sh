f77e6916fd99f06238a6935806352f989e4e7dc9
abdulfrfr <abdulmails112@gmail.com>
2024-03-26 14:29:00 +0100

    test

--- capestone-chart/kubeseal.sh
+++ capestone-chart/kubeseal.sh
@@ -12,3 +12,10 @@ fi
 wget "https://github.com/bitnami-labs/sealed-secrets/releases/download/v${KUBESEAL_VERSION}/kubeseal-${KUBESEAL_VERSION}-linux-amd64.tar.gz"
 tar -xvzf kubeseal-${KUBESEAL_VERSION}-linux-amd64.tar.gz kubeseal
 sudo install -m 755 kubeseal /usr/local/bin/kubeseal
+
+
+
+#Install sealed-secret using helm
+helm repo add sealed-secrets https://bitnami-labs.github.io/sealed-secrets
+
+helm install sealed-secrets -n kube-system --set-string fullnameOverride=sealed-secrets-controller sealed-secrets/sealed-secrets

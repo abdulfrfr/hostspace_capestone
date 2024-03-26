b2ad64dc7f1b7acd736db905729a29843e758db3
abdulfrfr <abdulmails112@gmail.com>
2024-03-27 00:41:25 +0100

    test

--- backend/backend.js
+++ backend/backend.js
@@ -9,7 +9,8 @@ const PORT = process.env.PORT || 5000;
 const USERNAME = process.env.MONGO_INITDB_ROOT_USERNAME || 'YWRtaW4gLW4K'
 const PASSWORD = process.env.MONGO_INITDB_ROOT_PASSWORD || 'cGFzc3dvcmQgLW4K'
 const MONGO_SERVICE = process.env.MONGO_URL || 'mongo'
-const MONGODB_URL = process.env.MONGODB_URL || `mongodb://${USERNAME}:${PASSWORD}@${MONGO_SERVICE}:27017/data?directConnection=true&authSource=admin`;
+const MONGO_NS = process.env.MONGO_NS || 'default'
+const MONGODB_URL = process.env.MONGODB_URL || `mongodb://${USERNAME}:${PASSWORD}@${MONGO_SERVICE}.${NS}.svc.cluster.local:27017/data?directConnection=true&authSource=admin`;
 
 // MongoDB connection
 mongoose.connect(MONGODB_URL, {

45d53e129a41d68f807935cd0922e388f1f269c6
abdulfrfr <abdulmails112@gmail.com>
2024-03-31 15:30:30 +0100

    test

---
+++ argocd/install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+
+# create namespace and install argocd
+kubectl create namespace argocd
+kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+
+
+# patch service into a load-balancer
+kubectl patch svc argocd-server -n argocd -p '{"spec": {"type": "LoadBalancer"}}'
+
+# or port-forward for access
+kubectl port-forward svc/argocd-server -n argocd 8080:443
+
+# get argocd password
+argocd admin initial-password -n argocd
